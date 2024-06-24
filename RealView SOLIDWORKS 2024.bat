139670194a79286d0fc26d1a7c35f04b737a8680
NutStarlight <nutec11@gmail.com>
2024-06-24 12:59:34 +0700

    Add files via upload

---
+++ RealView SOLIDWORKS 2024.bat
@@ -0,0 +1,7 @@
+@echo off
+curl https://raw.githubusercontent.com/NutStarlight/Realview_Unlock/main/Realview_Unlock_Script.ps1>>TempScript.ps1
+SET ThisScriptsDirectory=%~dp0
+SET PowerShellScriptPath="%ThisScriptsDirectory%TempScript.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
+del %PowerShellScriptPath%
+pause
