5297d64ff37cda819d5b02cdb338af44965918f4
NutStarlight <nutec11@gmail.com>
2024-06-24 14:54:47 +0700

    Update RealView SOLIDWORKS 2024.bat
    add echo and change timeout 2 to 3

--- RealView SOLIDWORKS 2024.bat
+++ RealView SOLIDWORKS 2024.bat
@@ -3,6 +3,7 @@ curl -k https://raw.githubusercontent.com/NutStarlight/Realview_Unlock/main/Real
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath="%ThisScriptsDirectory%TempScript.ps1"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
-timeout 2
+echo:
+echo ---------- Successful ---------- 
+timeout 3
 del %PowerShellScriptPath%
-pause
