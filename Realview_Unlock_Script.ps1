525b4a29eba9330092ac4018cce2093c203870ed
NutStarlight <nutec11@gmail.com>
2024-06-24 14:10:34 +0700

    Update Realview_Unlock_Script.ps1
    change '32408' to '205832'

--- Realview_Unlock_Script.ps1
+++ Realview_Unlock_Script.ps1
@@ -2,4 +2,4 @@ $pathCurrentVAG = 'HKCU:\SOFTWARE\SolidWorks\AllowList\Current'
 $nameVGA = (Get-ItemPropertyValue $pathCurrentVAG -Name Renderer)
 $path = 'SOFTWARE\SolidWorks\AllowList\Gl2Shaders\NV40'
 (Get-Item HKCU:).OpenSubKey($path,$true).CreateSubKey("$nameVGA")
-New-ItemProperty -Path "HKCU:\$Path\$nameVGA" -PropertyType DWORD -Value '0x32408' -Name 'Workarounds' -Force
+New-ItemProperty -Path "HKCU:\$Path\$nameVGA" -PropertyType DWORD -Value '205832' -Name 'Workarounds' -Force
