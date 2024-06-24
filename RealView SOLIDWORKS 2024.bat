0e995008b4ce0d7ce3de84cf2b7a6e9502bac685
NutStarlight <nutec11@gmail.com>
2024-06-24 13:42:21 +0700

    Update RealView SOLIDWORKS 2024.bat
    curl -k for windows 11
    ref : https://github.com/metaplex-foundation/metaplex/issues/2140

--- RealView SOLIDWORKS 2024.bat
+++ RealView SOLIDWORKS 2024.bat
@@ -1,5 +1,5 @@
 @echo off
-curl https://raw.githubusercontent.com/NutStarlight/Realview_Unlock/main/Realview_Unlock_Script.ps1>>TempScript.ps1
+curl -k https://raw.githubusercontent.com/NutStarlight/Realview_Unlock/main/Realview_Unlock_Script.ps1>>TempScript.ps1
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath="%ThisScriptsDirectory%TempScript.ps1"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
