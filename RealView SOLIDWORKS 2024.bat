4038a813d8123375c71640accdad3b304ea01251
NutStarlight <nutec11@gmail.com>
2024-06-24 13:07:35 +0700

    Update RealView SOLIDWORKS 2024.bat

--- RealView SOLIDWORKS 2024.bat
+++ RealView SOLIDWORKS 2024.bat
@@ -3,5 +3,6 @@ curl https://raw.githubusercontent.com/NutStarlight/Realview_Unlock/main/Realvie
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath="%ThisScriptsDirectory%TempScript.ps1"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
+timeout 2
 del %PowerShellScriptPath%
 pause
